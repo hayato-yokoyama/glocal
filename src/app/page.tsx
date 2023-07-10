a8234f29d125848b4feb0a118062a3f5f84ed1e4
dovicie <hyokoyama8810@gmail.com>
2023-07-11 08:58:46 +0900

    💄 topページのpagesのUIを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="form-control w-full">
       <label className="label">
-        <span className="label-text">どこに行く?</span>
+        <span className="label-text">場所</span>
       </label>
       <input
         type="text"
@@ -12,19 +12,32 @@ export default function Home() {
       <button className="btn">現在地からはこちら</button>
       <div className="form-control w-full">
         <label className="label">
-          <span className="label-text">ここからどのぐらい距離ある?</span>
+          <span className="label-text">から</span>
         </label>
         <select className="select-bordered select">
-          <option disabled selected>
-            200m
-          </option>
+          <option>200m</option>
           <option>500m</option>
           <option>800m</option>
           <option>1km</option>
           <option>3km</option>
           <option>10km</option>
         </select>
+        <label className="label">
+          <span className="label-text-alt">圏内</span>
+        </label>
       </div>
+
+      <div className="form-control w-full max-w-xs">
+        <label className="label">
+          <span className="label-text">キーワード</span>
+        </label>
+        <input
+          type="text"
+          placeholder="オムライス"
+          className="input-bordered input w-full max-w-xs"
+        />
+      </div>
+
       <fieldset>
         <legend>ジャンル</legend>
 
@@ -49,6 +62,12 @@ export default function Home() {
           </label>
         </div>
       </fieldset>
+      <div className="form-control">
+        <label className="label cursor-pointer">
+          <input type="checkbox" className="checkbox" />
+          <span className="label-text">営業中のスポットのみを表示</span>
+        </label>
+      </div>
       <button className="btn">この条件で探す</button>
     </div>
   );
