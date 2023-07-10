c28b7bfea6c877721c31e55bd9a5781d40c46d5c
dovicie <hyokoyama8810@gmail.com>
2023-07-11 08:38:37 +0900

    💄 topページの入力フォーム 作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,55 @@
 export default function Home() {
   return (
-    <>
-      <header>
-        <div className="navbar rounded-box bg-base-300">
-          <div className="flex-1 px-2 lg:flex-none">
-            <a className="text-xl font-bold">Glocal</a>
-          </div>
-          <div className="flex flex-none">
-            <div className="flex items-stretch">
-              <div className="dropdown-end dropdown">
-                <button tabIndex={0} className="btn-ghost btn-square btn">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    className="inline-block h-5 w-5 stroke-current"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                    ></path>
-                  </svg>
-                </button>
-                <ul
-                  tabIndex={0}
-                  className="dropdown-content menu rounded-box mt-4 w-52 bg-base-100 p-2 shadow"
-                >
-                  <li>
-                    <a>このアプリについて</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+    <div className="form-control w-full">
+      <label className="label">
+        <span className="label-text">どこに行く?</span>
+      </label>
+      <input
+        type="text"
+        placeholder="新宿駅"
+        className="input-bordered input w-full"
+      />
+      <button className="btn">現在地からはこちら</button>
+      <div className="form-control w-full">
+        <label className="label">
+          <span className="label-text">ここからどのぐらい距離ある?</span>
+        </label>
+        <select className="select-bordered select">
+          <option disabled selected>
+            200m
+          </option>
+          <option>500m</option>
+          <option>800m</option>
+          <option>1km</option>
+          <option>3km</option>
+          <option>10km</option>
+        </select>
+      </div>
+      <fieldset>
+        <legend>ジャンル</legend>
+
+        <div>
+          <label>
+            <input type="checkbox" />
+            レストラン
+          </label>
         </div>
-      </header>
-      <main>メイン</main>
-    </>
+
+        <div>
+          <label>
+            <input type="checkbox" />
+            観光地
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" />
+            カフェ
+          </label>
+        </div>
+      </fieldset>
+      <button className="btn">この条件で探す</button>
+    </div>
   );
 }
