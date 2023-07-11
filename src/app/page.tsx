d47372b0719d64a316bb5766cc3fefc1487a6dee
dovicie <hyokoyama8810@gmail.com>
2023-07-11 09:17:25 +0900

    💄 pagesのUI更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,35 @@
 export default function Home() {
   return (
-    <div className="form-control w-full">
-      <label className="label">
-        <span className="label-text">場所</span>
-      </label>
+    <div className="form-control w-full gap-y-2">
+      <label className="label">場所</label>
       <input
         type="text"
         placeholder="新宿駅"
         className="input-bordered input w-full"
       />
-      <button className="btn">現在地からはこちら</button>
-      <div className="form-control w-full">
-        <label className="label">
-          <span className="label-text">から</span>
-        </label>
-        <select className="select-bordered select">
-          <option>200m</option>
-          <option>500m</option>
-          <option>800m</option>
-          <option>1km</option>
-          <option>3km</option>
-          <option>10km</option>
+      <div className="flex items-center justify-end gap-x-2">
+        <button className="btn">現在地からはこちら</button>
+        <select className="select-bordered select w-52">
+          <option>200m（徒歩2分）</option>
+          <option>500m（徒歩6分）</option>
+          <option>800m（徒歩10分）</option>
+          <option>1km（自動車5分）</option>
+          <option>3km（自動車8分）</option>
+          <option>10km（自動車15分）</option>
         </select>
-        <label className="label">
-          <span className="label-text-alt">圏内</span>
-        </label>
       </div>
 
-      <div className="form-control w-full max-w-xs">
-        <label className="label">
-          <span className="label-text">キーワード</span>
-        </label>
+      <div className="form-control w-full">
+        <label className="label">キーワード</label>
         <input
           type="text"
           placeholder="オムライス"
-          className="input-bordered input w-full max-w-xs"
+          className="input-bordered input w-full"
         />
       </div>
 
       <fieldset>
         <legend>ジャンル</legend>
-
         <div>
           <label>
             <input type="checkbox" />
@@ -62,11 +51,9 @@ export default function Home() {
           </label>
         </div>
       </fieldset>
-      <div className="form-control">
-        <label className="label cursor-pointer">
-          <input type="checkbox" className="checkbox" />
-          <span className="label-text">営業中のスポットのみを表示</span>
-        </label>
+      <div className="flex items-center justify-center gap-x-2">
+        <input type="checkbox" className="checkbox" />
+        <span>営業中のスポットのみを表示</span>
       </div>
       <button className="btn">この条件で探す</button>
     </div>
