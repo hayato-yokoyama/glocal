29e8d93f632246ac02cae2ef1c44e58373c3bb54
dovicie <hyokoyama8810@gmail.com>
2023-07-31 09:28:27 +0900

    ⚡️   React Hook Formのインストールと既存フォームに適用させた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const GENRE_LIST = [
   "レストラン",
@@ -14,18 +13,20 @@ const GENRE_LIST = [
   "ホテル",
 ];
 
-export default function Home() {
-  const [place, setPlace] = useState("");
-  const handleChangePlace = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPlace(e.target.value);
-  };
+type filtering = {
+  distance: number;
+  genre: string[];
+  isOpen: boolean;
+  keyword: string;
+  place: string;
+};
 
-  const handleClickCurrentLocation = () => {
-    setPlace("現在地");
-  };
+export default function Home() {
+  const { register, handleSubmit } = useForm<filtering>();
+  const onSubmit = (data: filtering) => alert(JSON.stringify(data));
 
   return (
-    <div className="flex flex-col gap-y-8">
+    <form className="flex flex-col gap-y-8" onSubmit={handleSubmit(onSubmit)}>
       <fieldset className="flex flex-col gap-y-2">
         <legend className="flex items-center gap-x-2 px-1 py-2 font-bold">
           <Image src="pin.svg" width={32} height={32} alt="" />
@@ -35,28 +36,29 @@ export default function Home() {
           <input
             type="text"
             placeholder="新宿駅"
-            value={place}
-            onChange={handleChangePlace}
+            {...register("place", { required: true })}
             className="w-7 flex-1 rounded-lg border-2 border-stone-500 p-2 focus:border-accent focus:outline-none"
           />
         </div>
         <div className="ml-auto flex items-center gap-x-2">
-          <button
-            onClick={handleClickCurrentLocation}
-            className="border-lg m-auto rounded-full border-2 border-primary-400 bg-white px-4 py-2 text-xs"
-          >
+          {/* TODO: 現在地ボタンをクリックすると現在地が検索ワードに入るようにする */}
+          <button className="border-lg m-auto rounded-full border-2 border-primary-400 bg-white px-4 py-2 text-xs">
             現在地検索
           </button>
-          <select className="w-fit rounded-lg  border-2 border-stone-500 p-2 focus:border-accent focus:outline-none">
-            <option>200m（徒歩2分）</option>
-            <option>500m（徒歩6分）</option>
-            <option>800m（徒歩10分）</option>
-            <option>1km（自動車5分）</option>
-            <option>3km（自動車8分）</option>
-            <option>10km（自動車15分）</option>
+          <select
+            {...register("distance", { value: 800 })}
+            className="w-fit rounded-lg  border-2 border-stone-500 p-2 focus:border-accent focus:outline-none"
+          >
+            <option value={200}>200m（徒歩2分）</option>
+            <option value={500}>500m（徒歩6分）</option>
+            <option value={800}>800m（徒歩10分）</option>
+            <option value={1000}>1km（自動車5分）</option>
+            <option value={3000}>3km（自動車8分）</option>
+            <option value={10000}>10km（自動車15分）</option>
           </select>
         </div>
       </fieldset>
+
       <fieldset>
         <legend className="flex items-center gap-x-2 px-1 py-2 font-bold">
           <Image src="pen.svg" width={32} height={32} alt="" />
@@ -64,9 +66,11 @@ export default function Home() {
         </legend>
         <input
           type="text"
+          {...register("keyword")}
           className="w-full rounded-lg border-2 border-stone-500 p-2 focus:border-accent focus:outline-none"
         />
       </fieldset>
+
       <fieldset>
         <legend className="flex items-center gap-x-2 px-1 py-2 font-bold">
           <Image src="cup.svg" width={32} height={32} alt="" />
@@ -76,7 +80,12 @@ export default function Home() {
           {GENRE_LIST.map((genre, index) => {
             return (
               <label key={index}>
-                <input type="checkbox" className="peer hidden" />
+                <input
+                  type="checkbox"
+                  className="peer hidden"
+                  {...register("genre")}
+                  value={genre}
+                />
                 <span className="inline-block rounded-full border-2 border-primary-400 bg-white px-3 py-2 text-sm peer-checked:bg-primary-400 peer-checked:font-bold">
                   {genre}
                 </span>
@@ -85,17 +94,23 @@ export default function Home() {
           })}
         </div>
       </fieldset>
+
       <label className="flex justify-center gap-2">
-        <input type="checkbox" className="accent-primary-400" />
+        <input
+          type="checkbox"
+          className="accent-primary-400"
+          {...register("isOpen")}
+        />
         営業中のスポットのみを表示
       </label>
-      <Link
-        href="/search"
+
+      <button
+        type="submit"
         className="flex items-center justify-center gap-x-2 rounded-full bg-secondary p-4 font-bold"
       >
         <Image src="search.svg" width={24} height={24} alt="" />
         この条件で探す
-      </Link>
-    </div>
+      </button>
+    </form>
   );
 }
