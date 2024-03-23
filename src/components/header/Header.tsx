136f0d6577773348720ff4e87f59215aca175a39
dovicie <hyokoyama8810@gmail.com>
2024-03-23 11:02:28 +0900

    💄 HeaderのUI修正

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-slate-200">
+    <header>
       <div className="mx-auto flex h-16 max-w-sm items-center justify-start md:max-w-md xl:max-w-xl">
         <Title order={1}>
-          <Anchor component={Link} href="/" size="xl" underline="never">
+          <Anchor component={Link} href="/" size="2xl" underline="never">
             Glocal
           </Anchor>
         </Title>
