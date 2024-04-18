56936a44781f5dbb01224b8f03ad5894159e9c60
dovicie <hyokoyama8810@gmail.com>
2024-04-18 10:39:13 +0900

    🚧 緯度経度をuseStateで管理して、非同期で更新させるようにした

--- src/components/search/SearchList.tsx
+++ src/components/search/SearchList.tsx
@@ -3,33 +3,101 @@
 import SearchNotFoundPlace from "@/components/search/SearchNotFoundPlace";
 import { useLatLng } from "@/hooks/useLatLng";
 import { SearchParams } from "@/types/common";
+import { PlaceResult, PlaceSearchResponse } from "@/types/googleMapApi";
+import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 type SearchListProps = {
   searchParams: SearchParams;
 };
 
-const SearchList = ({ searchParams }: SearchListProps) => {
-  const { data, error, isEmpty, isLoading } = useLatLng(searchParams.place);
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  if (isLoading) {
-    return <p>loading</p>;
+const useSearchPlaces = (lat: number, lng: number, searchParams: SearchParams) => {
+  const [places, setPlaces] = useState<PlaceResult[]>([]);
+  const [nextPageToken, setNextPageToken] = useState<null | string>(null);
+
+  const getUrl = (token?: string) => {
+    const url = new URL(
+      `${
+        process.env.NEXT_PUBLIC_BASE_URL
+      }/api/searchPlace?lat=${lat}&lng=${lng}&distance=${searchParams.distance.toString()}`
+    );
+    if (searchParams.keyword) {
+      url.searchParams.append("keyword", searchParams.keyword);
+    }
+    if (searchParams.genre) {
+      url.searchParams.append("genre", searchParams.genre);
+    }
+    if (searchParams.isOpen) {
+      url.searchParams.append("isOpen", "true");
+    }
+    if (token) {
+      url.searchParams.append("token", token);
+    }
+    return url;
+  };
+
+  const { data, error, isLoading } = useSWR<PlaceSearchResponse, Error>(
+    nextPageToken ? getUrl(nextPageToken) : getUrl(),
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data && data.next_page_token) {
+      setNextPageToken(data.next_page_token);
+    } else {
+      setNextPageToken(null);
+    }
+    setPlaces((prevData) => [...prevData, ...(data ? data.results : [])]);
+  }, [data]);
+
+  return { data: places, error, isLoading };
+};
+
+const SearchList = ({ searchParams }: SearchListProps) => {
+  const [latLng, setLatLng] = useState({ lat: 0, lng: 0 });
+  // const [searchPlaces, setSearchPlaces] = useState<PlaceResult[]>([]);
+
+  const {
+    data: latLngData,
+    error: latLngError,
+    isEmpty: isLatLngEmpty,
+    isLoading: isLatLngLoading,
+  } = useLatLng(searchParams.place);
+
+  // const {
+  //   data: searchPlacesData,
+  //   error: searchPlacesError,
+  //   isLoading: isSearchPlacesError,
+  // } = useSearchPlaces(latLng.lat, latLng.lng, searchParams);
+
+  useEffect(() => {
+    console.log("LATLNG");
+    if (latLngData && !isLatLngEmpty) {
+      setLatLng(latLngData.results[0].geometry.location);
+    }
+  }, [latLngData, isLatLngEmpty]);
+
+  // useEffect(() => {
+  //   console.log("PLACES");
+  //   setSearchPlaces(searchPlacesData);
+  // }, [latLng]);
+
+  if (isLatLngLoading) {
+    return <p>緯度経度 検索中</p>;
   }
-  if (error || !data) {
-    return <p>エラーです</p>;
-  }
-  if (isEmpty) {
+  if (isLatLngEmpty) {
     return <SearchNotFoundPlace place={searchParams.place} />;
   }
-
-  /** 緯度経度 { lat:緯度 lng:経度 } */
-  const latLng = {
-    lat: data.results[0].geometry.location.lat,
-    lng: data.results[0].geometry.location.lng,
-  };
 
   return (
     <div>
       <p>{`緯度${latLng.lat} 軽度${latLng.lng}`}</p>
+      {/* <div>
+        <p>{searchPlaces[0] ? searchPlaces[0].name : ""}</p>
+        <p>{searchPlaces[1] ? searchPlaces[1].name : ""}</p>
+      </div> */}
     </div>
   );
 
