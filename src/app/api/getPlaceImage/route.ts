167721bbd28ce7c99010d5490e886a9bd05f1074
dovicie <hyokoyama8810@gmail.com>
2024-05-03 14:56:40 +0900

    🚧 ログ確認

--- src/app/api/getPlaceImage/route.ts
+++ src/app/api/getPlaceImage/route.ts
@@ -5,5 +5,6 @@ export async function GET(request: Request) {
   const photoReference = searchParams.get("photo_reference");
 
   const url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=560&photoreference=${photoReference}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
+  console.log("🚀 ~ GET ~ url:", url);
   redirect(url);
 }
